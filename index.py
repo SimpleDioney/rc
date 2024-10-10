72feadd3fed57ec190be21dba390b23df74d1b91
Dioney Gabriel <62361235+SimpleDioney@users.noreply.github.com>
2024-10-10 19:07:59 -0300

    Update index.py

--- index.py
+++ index.py
@@ -321,12 +321,14 @@ def get_content(url, timeout=5):
         print(f"Usando cache para {url}")
         return BeautifulSoup(cached_content, "html.parser")
 
-    headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
+   headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
     'Accept-Encoding': 'gzip, deflate, br',
-    'Referer': 'https://redecanais.tw',
     'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Cache-Control': 'max-age=0',
 }
     start_time = time.time()
     try:
